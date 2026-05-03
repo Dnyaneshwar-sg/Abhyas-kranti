1f6a7983f4ec7e92e846f878dd3be17782041074
Dnyaneshwar-sg <dsg150379@gmail.com>
2026-05-03 11:18:13 +0530

    Create aap.py

---
+++ aap.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def main():
+    st.set_page_config(page_title="अभ्यास क्रांती")
+    st.title("📚 अभ्यास क्रांती")
+    st.subheader("स्पर्धा परीक्षा तयारी केंद्र")
+    # ... तुमचा इतर कोड ...
+
+if __name__ == '__main__':
+    main()
