00c3c8421b3db4b389f90572823963784c7a135c
Dnyaneshwar-sg <dsg150379@gmail.com>
2026-05-13 23:57:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -257,10 +257,9 @@ if send and user_input:
         "content": reply
     })
 
-  with st.chat_message("assistant"):
+with st.chat_message("assistant"):
     print(reply)
     st.write(reply)
-
 
 # STUDY PLANNER
 # ------------------------
