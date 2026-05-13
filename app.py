cf68a61768569af77b02b6fd7d66189c85ada41f
Dnyaneshwar-sg <dsg150379@gmail.com>
2026-05-13 22:28:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -270,46 +270,7 @@ st.session_state.messages.append({
 with st.chat_message("assistant"):
     st.write(reply)
 
-st.session_state.messages.append({
-    "role": "assistant",
-    "content": reply
-})
 
-with st.chat_message("assistant"):
-    st.write(reply)
-    model = genai.GenerativeModel("gemini-1.5-flash")
-
-
-
-
-user_input = st.chat_input("Ask your question")
-
-if user_input:
-    response = model.generate_content(user_input)
-    reply = response.text
-
-    st.write(reply)
-
-
-
-st.session_state.messages.append({
-    "role": "assistant",
-    "content": reply
-})
-
-with st.chat_message("assistant"):
-    st.write(reply)
-     
-# -----------------------------
-# STUDY PLANNER
-# -----------------------------
-st.session_state.messages.append({
-    "role": "assistant",
-    "content": reply
-})
-
-with st.chat_message("assistant"):
-    st.write(reply)# ------------------------
 # STUDY PLANNER
 # ------------------------
 
