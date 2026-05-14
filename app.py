8993358ce1387f565281a013fcfd839dd8962818
Dnyaneshwar-sg <dsg150379@gmail.com>
2026-05-14 10:22:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -239,9 +239,11 @@ if send and user_input:
         "role": "user",
         "content": user_input
     })
-genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+    genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
+
+    model = genai.GenerativeModel("gemini-1.5-flash")
+
     response = model.generate_content(user_input)
 
     reply = response.text
