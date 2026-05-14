a5de0c639e46b57f3c5656f74b981cfadd660a00
Dnyaneshwar-sg <dsg150379@gmail.com>
2026-05-14 15:34:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -233,9 +233,10 @@ elif selected == "AI Mentor":
 
         reply = response.text
 
- with st.chat_message("assistant"):
+with st.chat_message("assistant"):
     st.write(reply)
-  elif selected == "Study Planner":
+
+elif selected == "Study Planner":
         
     st.title("📅 Smart Study Planner")
 
