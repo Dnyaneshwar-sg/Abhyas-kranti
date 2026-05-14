821c2b33610d91984713b4972b35c2256fd9c4b3
Dnyaneshwar-sg <dsg150379@gmail.com>
2026-05-14 12:48:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -235,18 +235,22 @@ if send and user_input:
             "role": "user",
             "content": user_input
         })
-
-        genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
         model = genai.GenerativeModel("gemini-1.5-flash")
-   response = model.generate_content(user_input)
-   reply = response.text
-       
-          
+
+        response = model.generate_content(user_input)
+
+        reply = response.text
+
+        with st.chat_message("assistant"):
+            st.write(reply)
+
         st.session_state.messages.append({
             "role": "assistant",
             "content": reply
         })
+     
 # STUDY PLANNER
 # ------------------------
 
