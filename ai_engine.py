7f9a18785598415905f08647dbff91ac28cae833
Dnyaneshwar-sg <dsg150379@gmail.com>
2026-05-28 18:00:58 +0530

    Update ai_engine.py

--- ai_engine.py
+++ ai_engine.py
@@ -1,1 +1,23 @@
 
+import google.generativeai as genai
+import streamlit as st
+
+genai.configure(
+    api_key=st.secrets["GEMINI_API_KEY"]
+)
+
+model = genai.GenerativeModel(
+    "gemini-1.5-flash"
+)
+
+def ask_ai(question):
+
+    try:
+
+        response = model.generate_content(question)
+
+        return response.text
+
+    except Exception as e:
+
+        return f"Error: {str(e)}"
